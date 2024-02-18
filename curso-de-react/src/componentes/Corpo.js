79a872d8dceac8f8f290805a62f1cba7fdc91da4
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-02-18 11:12:01 -0300

    feita aula 07
    enviar funções para componentes.

--- curso-de-react/src/componentes/Corpo.js
+++ curso-de-react/src/componentes/Corpo.js
@@ -4,13 +4,22 @@ import Dados from './Dados'
 function Corpo() {
 
     const cnl = 'cfb cursos'
-    const yt = 'cfb cursos'
+
+    const yt=()=>{
+
+        return 'youtube.com/cfbcursos'   
+
+    }
+
+    const somar = (v1,v2)=>{
+        return v1 + v2
+    }
 
     return(
         <section>
             <h2>Curso de react</h2>
             <p>se inscreva no canal</p>
-            <Dados canal={cnl} youtube={yt} curso='react.js'></Dados>
+            <Dados canal={cnl} youtube={yt} curso='react.js' somar={somar}></Dados>
         </section>
     )
 }
--- curso-de-react/src/componentes/Dados.js
+++ curso-de-react/src/componentes/Dados.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Dados(props){
+
+    const n1 = 10
+    const n2 = 5
+
     return(
         <div>
-            <p>canal:{props.canal}</p>
-            <p>youtube:{props.youtube}</p>
-            <p>curso:{props.curso}</p>
+            <p>canal: {props.canal}</p>
+            <p>youtube: {props.youtube()}</p>
+            <p>curso: {props.curso}</p>
+            <p>{`a soma de ${n1} com ${n2} é igual a: ${props.somar(n1,n2)}`}</p>
         </div>
     )
 }
