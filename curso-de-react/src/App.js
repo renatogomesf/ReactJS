726f34a5d60be824099a6993886915a91c1a6d2e
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-02-23 10:06:53 -0300

    feita aula 12
    renderização condicionla

--- curso-de-react/src/App.js
+++ curso-de-react/src/App.js
@@ -1,5 +1,4 @@
 import React,{useState} from 'react'
-import Led from './componentes/Led';
 
 
 // quando a CSS é importada no arquivo principal, não precisa importar de novo nos componentens.
@@ -7,23 +6,48 @@ import './App.css'
 
 
 function App() {
-  
-  const [ligado,setLigado]=useState(false)
 
-  const cancelar = (obj)=>{
-    return obj.preventDefault()
+  const [cor,setCor] = useState(1)
+
+  const vermelho = {color: '#f00'}
+  const verde = {color: '#0f0'}
+  const azul = {color: '#00f'}
+
+  const retCor=(c)=>{
+    if(c==1){
+      return vermelho
+    }else if(c==2){
+      return verde
+    }else{
+      return azul
+    }
   }
+
+  const mudaCor=()=>{
+    setCor(cor+1)
+
+    if(cor > 2){
+      setCor(1)
+    }
+  }
+
+  const [horario,setHora] = useState()
+
+  const hora=()=>{
+    setHora(new Date().toLocaleTimeString())
+  }
+
+  setInterval(hora,1000)
 
   return (
     <div className="App">
-      <Led ligado={ligado} setLigado={setLigado}></Led>
+      
+      <h1 style={retCor(cor)} >cfb cursos</h1>
 
-      <a 
-      href="http://youtube.com/cfbcursos"
-      target='blank'
-      onClick={(e)=>cancelar(e)}>
-        cfb cursos
-      </a>
+      <button onClick={()=>mudaCor()}>mudar cor</button>
+
+      <p>{horario}</p>
+
     </div>
   );
 }
