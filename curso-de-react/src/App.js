5297c3af4846b255ff4b54b79799ce93c8823b96
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-02-27 10:20:05 -0300

    revisando aula 13

--- curso-de-react/src/App.js
+++ curso-de-react/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import RenderCondicional from './AULAS/aula12/RenderCondicional';
+import Lista from './AULAS/aula13/Lista'
 
 // quando a CSS é importada no arquivo principal, não precisa importar de novo nos componentens.
 import './App.css'
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       
-      <RenderCondicional></RenderCondicional>
+      <Lista></Lista>
       
     </div>
   );
